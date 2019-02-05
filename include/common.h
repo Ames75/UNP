b23d70e7e0a388845d9fd9f81fbd7f88596cefbc
Ames75 <orc.leaner@gmail.com>
2019-02-04 23:28:03 -0500

    common.h changes

--- include/common.h
+++ include/common.h
@@ -11,7 +11,32 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <iostream>
+#include <string>
+//#include <sys/select.h>
+
 
 #define MAXLINE 4096
 #define LISTENQ 256
+#define ECHO_PORT 1313
+
+void err_sys(const char* x) 
+{ 
+    perror(x); 
+    exit(1); 
+}
+
+// return true if we should exit the program
+bool handle_error(const std::string& place) {
+	std::cout<<place<<" failure due to " << strerror(errno) << std::endl;
+	return false;
+}
+
+// return true if we should exit the program
+bool handle_error(const char* place) {
+	std::cout<<place<<" failure due to " << strerror(errno) << std::endl;
+	return false;
+}
+
+
 #endif
