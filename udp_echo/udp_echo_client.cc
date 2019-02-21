2d8b5cc60a02da97505eee119e365f762d4879e1
Ames75 <orc.leaner@gmail.com>
2019-02-21 10:59:29 -0500

    using client connect

--- udp_echo/udp_echo_client.cc
+++ udp_echo/udp_echo_client.cc
@@ -10,17 +10,49 @@ static void udp_echo_client_proc(int sockfd, const sockaddr* servaddr,
 {
     int n;
     char sendbuffer[MAXLINE], recvbuffer[ MAXLINE + 1];
-    struct sockaddr_in fromAddr;
+    char fromIP[128];
+    struct sockaddr* fromAddr = (sockaddr*)malloc(servlen);
     socklen_t len = servlen;
     while(fgets(sendbuffer, MAXLINE, stdin) != nullptr) {
         sendto(sockfd, sendbuffer, strlen(sendbuffer), 0, servaddr, servlen);
         n = recvfrom(sockfd, recvbuffer, MAXLINE, 0, 
-                     (sockaddr*)&fromAddr, &len);
+                     (sockaddr*)fromAddr, &len);
         recvbuffer[n] = 0;
         fputs("I've received a message ",stderr);
         fputs(recvbuffer, stderr);
+        if(my_inet_ntop(fromAddr->sa_family,fromAddr,fromIP,sizeof(fromIP))) {
+            cout << "it is from " << fromIP 
+                 << " port " << my_getSockPort(fromAddr) << endl;
+        }      
+    }
+    free(fromAddr);
+}
+
+static void udp_echo_client_connect(int sockfd, const sockaddr* servaddr,
+                                         socklen_t servlen) 
+{
+    int n;
+    char sendbuffer[MAXLINE], recvbuffer[ MAXLINE + 1];
+    char fromIP[128];
+    connect(sockfd, const_cast<sockaddr*>(servaddr), servlen);
+    while(fgets(sendbuffer, MAXLINE, stdin) != nullptr) {
+        write(sockfd, sendbuffer, strlen(sendbuffer));
+        n = read(sockfd, recvbuffer, MAXLINE);
+        if( n < 0 ) {
+            handle_error("after read in udp_echo_client_connect");
+            continue;
+        }
+        recvbuffer[n] = 0;
+        fputs("I've received a message ",stderr);
+        fputs(recvbuffer, stderr);
+        if(my_inet_ntop(servaddr->sa_family,servaddr,fromIP,sizeof(fromIP))) {
+            cout << "it is from " << fromIP 
+                 << " port " << my_getSockPort(servaddr) << endl;
+        }      
     }
 }
+
+
 
 int main(int argc, char** argv) {
     int sockfd;
@@ -36,6 +68,7 @@ int main(int argc, char** argv) {
        return 1;
     }
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    udp_echo_client_proc(sockfd,(sockaddr*)&servaddr,sizeof(servaddr));
+    // udp_echo_client_proc(sockfd,(sockaddr*)&servaddr,sizeof(servaddr));
+    udp_echo_client_connect(sockfd,(sockaddr*)&servaddr,sizeof(servaddr));
     return 0;
 }
