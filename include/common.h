b4fb5718f412a40644f3b40d4d31b1f28113e116
Ames75 <orc.leaner@gmail.com>
2019-02-27 12:28:26 -0500

    move things to library

--- include/common.h
+++ include/common.h
@@ -17,7 +17,8 @@
 #include <limits.h>
 #include <poll.h>
 #include <signal.h>
-
+#include <getopt.h>
+#include <vector>
 
 /* Posix.1g requires that an #include of <poll.h> DefinE INFTIM, but many
    systems still DefinE it in <sys/stropts.h>.  We don't want to include
@@ -32,6 +33,8 @@
 #define MAXLINE 4096
 #define LISTENQ 256
 #define ECHO_PORT 1313
+
+#define CHECK_ARG(num)  if ( argc < num ) usage();
 
 void err_sys(const char* x) 
 { 
@@ -51,87 +54,23 @@ bool handle_error(const char* place) {
 	return false;
 }
 
-int my_bind(int sockfd, const struct sockaddr *addr,
-			   socklen_t addrlen) {
-	int result=0;
-	if ((result=bind(sockfd, (sockaddr*)addr, addrlen)) != 0) {
-		if(handle_error(std::string("bind in server"))) {
-			exit(1);
-		}
-	}
-	return result;
-}
+int my_bind(int sockfd, const struct sockaddr *addr, socklen_t addrlen);
 
-int my_inet_pton(int family, const char *strptr, void *addrptr) {
-    if(!inet_pton(family, strptr, addrptr)) {
-        std::cout<<"server ip address is wrong !"<<std::endl;
-        return 1;
-    }
-    return 0;
-};
+int my_inet_pton(int family, const char *strptr, void *addrptr);
 
 // here src is struct sockaddr
 const char* my_inet_ntop(int af, const sockaddr *src,
-                             char *dst, socklen_t size) {
-    if (af != AF_INET && af != AF_INET6) {
-        std::cout<<"address family type "<<af<<" not supported"<<std::endl;
-        return nullptr; 
-    }
-    const char* result;
-    if( af == AF_INET) {
-        result = inet_ntop(af,&((const sockaddr_in*)src)->sin_addr,dst, size);
-    } else {
-        result = inet_ntop(af,&((const sockaddr_in6*)src)->sin6_addr,dst, size);
-    }
-    
-    if(result == nullptr) {
-        handle_error("within my_inet_ntop");
-        exit(1);
-    }
-    return result;
-}
+                             char *dst, socklen_t size);
 
-uint16_t my_getSockPort(const sockaddr *src) {
-    int af = src->sa_family;
-    if (af != AF_INET && af != AF_INET6) {
-        std::cout<<"getSockPort:address family type "<<
-                 af<<" not supported"<<std::endl;
-        return 0; 
-    }
-    if (af == AF_INET) {
-        return htons(((const sockaddr_in*)src)->sin_port);
-    } else {
-        return htons(((const sockaddr_in6*)src)->sin6_port);
-    }
-}
+uint16_t my_getSockPort(const sockaddr *src);
 
-std::string my_sock_ntop(const struct sockaddr* src, socklen_t size) {
-    std::string result;
-    char ipString[128];
-    uint16_t port;
-    const char* tmp = my_inet_ntop(src->sa_family,
-                             src, ipString, sizeof(ipString));
-    if(tmp) result = std::string(tmp);
-    port = my_getSockPort(src);
-    if(port != 0) result += std::string(":")+std::to_string(port);
-    return result;
-}
+std::string my_sock_ntop(const struct sockaddr* src, socklen_t size);
 
-
-sighandler_t my_signal(int signum, sighandler_t handler) {
-    sighandler_t result;
-    if((result = signal(signum, handler)) ==  SIG_ERR) {
-        handle_error("in my_signal");
-    }
-    return result;
-}
+sighandler_t my_signal(int signum, sighandler_t handler);
 
 void
-my_getsockname(int fd, struct sockaddr *sa, socklen_t *salenptr)
-{
-	if (getsockname(fd, sa, salenptr) < 0)
-		handle_error("getsockname error");
-}
+my_getsockname(int fd, struct sockaddr *sa, socklen_t *salenptr);
+
 
 
 #endif
